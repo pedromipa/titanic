7fbbc04deeba289d70fb51bd06e914d6054636a8
pedromipa <pedro.mipa@hotmail.com>
2020-10-09 17:12:58 -0300

    example
    using an example

--- first.py
+++ first.py
@@ -2,5 +2,30 @@ import numpy as np
 import pandas as pd 
 
 train = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
 
-print(train.head())
+women = train.loc[train.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+
+print("% of women who survived:", rate_women)
+
+men = train.loc[train.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+
+print("% of men who survived:", rate_men)
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
