7e6cea1ca9f086079c0d901720ad3c9759d2157a
pedromipa <pedro.mipa@hotmail.com>
2020-10-13 16:36:51 -0300

    randomforest
    implementation of a random forest

---
+++ randomforest.py
@@ -0,0 +1,42 @@
+import numpy as np 
+import pandas as pd 
+
+train = pd.read_csv('train.csv')
+test =  pd.read_csv('test.csv')
+
+from sklearn.ensemble import RandomForestClassifier
+
+modelo = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
+
+def transformar_sexo(valor):
+    if valor == 'female':
+        return 1
+    else: 
+        return 0
+
+train['Sex_binario'] = train['Sex'].map(transformar_sexo)
+
+variaveis = ['Sex_binario','Age']
+
+x = train[variaveis]
+y = train['Survived']
+
+x = x.fillna(-1)
+#print(x.head())
+#print(y.head())
+
+modelo.fit(x,y)
+
+test['Sex_binario'] = test['Sex'].map(transformar_sexo)
+
+x_previsao = test[variaveis]
+
+x_previsao = x_previsao.fillna(-1)
+
+print(x_previsao.head())
+
+p = modelo.predict(x_previsao)
+
+submission = pd.Series(p, index = test['PassengerId'], name='Survived')
+
+submission.to_csv("Previsao_modelo.csv",header=True)
